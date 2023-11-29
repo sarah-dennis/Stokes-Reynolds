b88766fe3ecbc6819d847f0fab3c9bda20ae695a
sarahdennis-brandeis <sarahdennis@brandeis.edu>
2023-11-29 11:32:50 -0500

    is resistance different on another branch help hel

--- resistance.py
+++ resistance.py
@@ -14,7 +14,7 @@ from pressures import StepPressure
 import numpy as np
 # x boundary
 x0 = 0
-xf = 8
+xf = 6
 BC = "fixed" 
 
 #pressure boundary
@@ -34,56 +34,64 @@ Nx = 500
 domain = dm.Domain(x0, xf, visc, U, Nx, BC)
 
 #Height
-l1 = 3
+l1 = 2
 x_step = x0+l1
 l2 = xf-l1
 
 h1 = 1
-H_ratio = 1.5423 #expansion ratio 
+H_ratio = 1.9423 #expansion ratio 
 h2 = H_ratio * h1
 
 h_avg = (h1 + h2)/2
 r = abs(h1 - h2)/2
 height = StepHeight(domain, x_step, h_avg, r)
 
-#reynolds analytic solution
+#----------------------------------------------
+# Reynolds analytic solution
+
 p_reyn = StepPressure(domain, height, p0, pN)
 
 px1_reyn = p_reyn.m_in
 px2_reyn = p_reyn.m_out
+
 p_max_reyn = px1_reyn*l1+p0
 print("Reynolds \n px1: %.3f, px2: %.3f"%(px1_reyn, px2_reyn))
 print(" p_max: %.3f\n"%p_max_reyn)
 
+#----------------------------------------------
+
 # Flux 
 Q = (U * h1)/2 - (px1_reyn * (h1**3))/(12*visc) 
 print("Flux: Q=%.3f"%Q)
+
 # Bulk velocity
 Ub1 = Q/h1
 Ub2 = Q/h2 # = h1/h2 * Ub1
 
-# BISWAS (stokes) solution
-
-Re = 2*h1*rho*Ub1/visc #reynolds number
-
+#reynolds number
+Re = 2*h1*rho*Ub1/visc 
 print("Reynolds num: %.3f\n"%Re)
 
-px1_stokes = -lam*rho*l1*(Ub1**2)/(2*h1*Re*l1)   
-p_max_stokes_in = px1_stokes*l1+p0
+#----------------------------------------------
+# BISWAS (stokes) solution
 
+dp1_biswas = -lam*rho*l1*(Ub1**2)/(2*h1*Re)   
+dp2_biswas = -lam*rho*l2*(Ub2**2)/(2*h2*Re)
 
-px2_stokes = -lam*rho*l2*(Ub2**2)/(2*h2*Re*l2)
-p_max_stokes_out = -px2_stokes*(l2) + pN
+px1_biswas = dp1_biswas/l1
+px2_biswas = dp2_biswas/l2
 
+p_max_biswas = dp1_biswas+p0
 
-#adjust for expansion ratio
+# pressure drop adjusted for expansion ratio
 px_corr = (1-h1**2/h2**2)*(rho*Ub1**2)/2
 
-print("Biswas Stokes \n px1: %.3f, px2: %.3f "%(px1_stokes, px2_stokes))
+print("Biswas Stokes \n px1: %.3f, px2: %.3f "%(px1_biswas, px2_biswas))
+
 print(" px_correction: %.3f\n"%px_corr)
 
 print("hyd-res reyn: %.5f"%(-Q/(px1_reyn + px2_reyn)))
-print("hyd-res biswas: %.5f"%(-Q/(px1_stokes + px2_stokes - px_corr)))
+print("hyd-res biswas: %.5f"%(-Q/(px1_biswas + px2_biswas - px_corr)))
 
 def make_disc_ps(x0, l1, l2, m1, m2, Nx):
     ps = np.zeros(Nx)
@@ -100,7 +108,7 @@ def make_disc_ps(x0, l1, l2, m1, m2, Nx):
 
     return ps
 
-ps = make_disc_ps(x0, l1, l2, px1_stokes, px2_stokes, domain.Nx) #p_reyn.ps
+ps = make_disc_ps(x0, l1, l2, px1_biswas, px2_biswas, domain.Nx)
 
 #------------------------------------------------------------------------------
 # Pressure & Height plotting 
