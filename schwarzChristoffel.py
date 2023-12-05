563f1b910468ff3b0d77e4a573cdd05a14ae2de1
sarahdennis-brandeis <sarahdennis@brandeis.edu>
2023-12-05 10:50:54 -0500

    flatten f(t)

--- schwarzChristoffel.py
+++ schwarzChristoffel.py
@@ -8,10 +8,44 @@ Created on Thu Nov 30 11:05:44 2023
 import numpy as np
 import graphics as graph
 
+#------------------------------------------------------------------------------    
+# t : Upper half plane
+#             .
+#             |
+#             |
+#             |
+# -infty ---- 0 ---- 1 --- a ---- +infty
+#  <-- L --- N=M --- P --- R ---- S -->
+
+# t = x + iy
+
+t_Nx = 20
+t_Ny = 20
+
+t_xMin = -10
+t_xMax = 10
+t_yMin = 0
+t_yMax = 10
+
+t_dx = (t_xMax - t_xMin)/(t_Nx-1)
+t_dy = (t_yMax - t_yMin)/(t_Ny-1)
+
+t_xs = np.zeros(t_Nx)
+t_ys = np.zeros(t_Ny)
+
+for i in range (t_Nx):
+    t_xs[i] = t_xMin + i*t_dx
+for j in range (t_Ny):
+    t_ys[j] = t_yMin + j*t_dy
+
+# t_plane = np.zeros((t_Ny, t_Nx), dtype=complex)
+# for i in range(t_Nx):
+#     for j in range(t_Ny):
+#         t_plane[j][i] = complex(t_xs[i], t_ys[j])
 
 #------------------------------------------------------------------------------
 # Pentagon
-
+#------------------------------------------------------------------------------
 # -  M ----------- L -
 # h1 |             | |
 # -  N ---- P      | h2
@@ -25,107 +59,78 @@ U=0.5
 l1 = 2 # dist N -> P
 l2 = 5 # dist R -> S
 
-h1 = 2 #dist N -> M
-h2 = 3 #dist S -> L
+h1 = 1 #dist N -> M
+h2 = 2 #dist S -> L
 
 # Step height : 
 #    h2 > h1, a = (H/h)**2
-
-#------------------------------------------------------------------------------
-# Conformally Transformed Plane     (t-plane)
-
-#              |      *
-#           *  |              *
-#       *      |                   *
-#    *         |                       *
-#              |    
-# L --------- M~N ---- P ------- R ----- S
-# t0 -(l1+l2)  0   +l1    +h2-h1    +l2 trad
-
-Nx = 50
-Ny = 50
-
-t_x0 = -(l1+l2)
-t_y_0 = 0
-
-t_xlen = 2*(l1+l2) + h2-h1
-t_ylen = t_xlen/2
-
-t_dx = t_xlen / (Nx-1)
-t_dy = t_ylen/ (Ny-1)
-
-t_plane = np.zeros((Ny, Nx), dtype=complex)
-
-for i in range(Nx):
-    x = t_x0 + i*t_dx
-    
-    for j in range(Ny):
-        y = j*t_dy
-        
-        t_plane[j][i] = complex(x,y)
-
-        
-#------------------------------------------------------------------------------    
-# Schwarz-Christoffel Mapping z := f(t)   (z-plane)
-        
+     
+#------------------------------------------------------------------------------   
+# # Schwarz-Christoffel Mapping z := f(t)   (z-plane)
+#------------------------------------------------------------------------------       
 a = (h2/h1)**2
 b1 = h2/np.pi 
 b2 = h1/np.pi
-c = complex(0,-np.pi*(1-h1/h2))
+c = complex(0, -h2+h1)
 
 def f(t):
-    arg1 = (2*t - (a+1))/(a-1)
+    
+    arg1 = (2*t-a-1)/(a-1)
     
     arg2 = ((a+1)*t - 2*a)/((a-1)*t)
     
     z = b1 * np.arccosh(arg1) - b2 * np.arccosh(arg2) + c
     
     return z
-    
-    
-z_plane = np.zeros((Ny, Nx), dtype=complex)
 
-for i in range(Nx):
-    for j in range (Ny):
-        z_plane[j][i]=f(t_plane[j][i])
+#---------------------------------------------------------------------------- 
+z_Nx = 20
+z_Ny = 20
+
+z_xs = np.zeros(z_Nx)
+z_ys = np.zeros(z_Ny)
+
+z_xMin = -l1
+z_xMax = l2
+z_yMin = h1-h2
+z_yMax = h2
+
+z_dx = np.abs(z_xMin - z_xMax)/(z_Nx-1)
+for i in range(z_Nx):
+    z_xs[i] = z_xMin + i*z_dx
+   
+z_dy = np.abs(z_yMin - z_yMax)/(z_Ny-1)
+for j in range(z_Ny):
+   z_ys[j] = z_yMin + j*z_dy
+   
+z_plane = np.zeros((z_Ny, z_Nx), dtype=complex)
+
+for i in range(z_Nx):
+    for j in range(z_Ny):
+        t = complex(t_xs[i], t_ys[j])
+        z = complex(z_xs[i], z_ys[j])
+        ft = f(t)
+        z_plane[j][i]= ft
+        print("z: (%.2f, %.2f), f(t): (%.2f, %.2f)" % (z.real, z.imag, ft.real, ft.imag))
+# print(z_plane)
     
 #------------------------------------------------------------------------------
 # Stream Function Mapping   s := phi(z)
+#------------------------------------------------------------------------------
 
-
-# --- for plotting make C grid ----
-xs = np.zeros(Nx)
-ys = np.zeros(Ny)
-
-z_x0 = -l1
-z_y0 = h1-h2
-
-z_dx = (l1 + l2)/(Nx-1)
-for i in range(Nx):
-    xs[i] = z_x0 + i*z_dx
-   
-z_dy = h2/(Ny-1)
-for j in range(Ny):
-   ys[j] = z_y0 + j*z_dy
-    
-#-----------------------------------
-# compute stream s = phi(z) 
-
-
-d = complex(0, U*h2/(2*np.pi))
+d = complex(0, -U*h2/(2*np.pi))
 def stream(z):
-       return d * np.log(z/z.conjugate())
+    return  d*np.log(z/z.conjugate())
    
-s_x = np.zeros((Ny, Nx))
-s_y = np.zeros((Ny, Nx))
+s_x = np.zeros((z_Ny, z_Nx))
+s_y = np.zeros((z_Ny, z_Nx))
 
 # mask with height function...
 
-for i in range(Nx):
-    for j in range (Ny):
-        if xs[i] >= 0 or ys[j] >= 0:
-        
-            s = stream(z_plane[j][i])
+for i in range(z_Nx):
+    for j in range (z_Ny):
+        if z_xs[i] >= 0 or z_ys[j] >= 0:
+            s = stream(complex(z_xs[i], z_ys[j]))
             s_x[j][i] = s.real
             s_y[j][i] = s.imag
 
@@ -136,10 +141,9 @@ for i in range(Nx):
 title = "Stream Plot"
 ax_labels = ["x", "y"]
 
+graph.plot_stream(s_x, s_y, z_xs, z_ys, title, ax_labels)
 
-# graph.plot_stream(s_x, s_y, xs, ys, title, ax_labels)
-
-graph.plot_quivers(s_x, s_y, xs, ys, title, ax_labels)
+graph.plot_quivers(s_x, s_y, z_xs, z_ys, title, ax_labels)
     
 
     
