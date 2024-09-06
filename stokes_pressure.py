9072cd6d19d726ba6e95dbd36f2b25033141f279
sarahdennis-brandeis <114016311+sarahdennis-brandeis@users.noreply.github.com>
2024-09-06 10:01:58 -0400

    pressure is broken
    issue with /slope=0, in EW- why is it happenenigng

--- stokes_pressure.py
+++ stokes_pressure.py
@@ -55,7 +55,7 @@ def fishfun(ex, u, v):
         i = k % n
         j = k // n
         
-        if space[j,i] == -1: # exterior
+        if space[j,i] != -1: # exterior & boundary
             continue
         else:
             
@@ -63,84 +63,46 @@ def fishfun(ex, u, v):
             v_k = v[k]
     
             # uxx & vxx <--| E:i+1 & W:i-1 
-            if i == 0 or space[j,i-1]==-1:
-                u_E = u[j*n + i+1]
-                u_2E = u[j*n + i+2]
-                u_3E = u[j*n + i+3]
-                
-                v_E = v[j*n + i+1]
-                v_2E = v[j*n + i+2]
-                v_3E = v[j*n + i+3]
-                
-                uxx_k = (2*u_k - 5*u_E + 4*u_2E - u_3E)/ex.dx**3
-                vxx_k = (2*v_k - 5*v_E + 4*v_2E - v_3E)/ex.dx**3
-                
-            elif i == n-1 or space[j,i+1] ==-1:
-                u_W = u[j*n + i-1]
-                u_2W = u[j*n + i-2]
-                u_3W = u[j*n + i-3]
-                
-                v_W = v[j*n + i-1]
-                v_2W = v[j*n + i-2]
-                v_3W = v[j*n + i-3]
-                
-                uxx_k = (2*u_k - 5*u_W + 4*u_2W - u_3W)/ex.dx**3
-                vxx_k = (2*v_k - 5*v_W + 4*v_2W - v_3W)/ex.dx**3
-                
+            if space[j,i+1]==-1:
+                u_E = ex.interp_E_W(i,j, i+1,j, u_k)
+                v_E = ex.interp_E_W(i,j, i+1,j, v_k)
             else:
-            
-
-                u_W = u[j*n + i-1]
-                v_W = v[j*n + i-1]
-                
                 u_E = u[j*n + i+1]
                 v_E = v[j*n + i+1]
                 
-                uxx_k = (u_E -2*u_k + u_W)/ex.dx**2
-                vxx_k = (v_E -2*v_k + v_W)/ex.dx**2
+            if space[j,i-1] ==-1:
+                u_W = ex.interp_E_W(i,j, i-1,j, u_k)
+                v_W = ex.interp_E_W(i,j, i-1,j, v_k)
+            else:
+                u_W = u[j*n + i-1]
+                v_W = v[j*n + i-1]
+                
+                
+                
+                
+            uxx_k = (u_E -2*u_k + u_W)/ex.dx**2
+            vxx_k = (v_E -2*v_k + v_W)/ex.dx**2
 
 
             
             # uyy & vyy <--| N:j+1 & S:j-1
+
+            u_N = u[(j+1)*n + i]                    
+            v_N = v[(j+1)*n + i]
             
-
-            if j==m-1:
-                u_S = u[(j-1)*n + i]
-                u_2S = u[(j-2)*n + i]
-                u_3S = u[(j-3)*n + i]
-                
-                v_S = v[(j-1)*n + i]
-                v_2S = v[(j-2)*n + i]
-                v_3S = v[(j-3)*n + i]
-                
-                uyy_k = (2*u_k - 5*u_S + 4*u_2S - u_3S)/ex.dy**3
-                vyy_k = (2*v_k - 5*v_S + 4*v_2S - v_3S)/ex.dy**3
-                
-            elif j==0 or space[j-1,i] == -1:
-                u_N = u[(j+1)*n + i]
-                u_2N = u[(j+2)*n + i]
-                u_3N = u[(j+3)*n + i]
-                
-                v_N = v[(j+1)*n + i]
-                v_2N = v[(j+2)*n + i]
-                v_3N = v[(j+3)*n + i]
-                
-                uyy_k = (2*u_k - 5*u_N + 4*u_2N - u_3N)/ex.dy**3
-                vyy_k = (2*v_k - 5*v_N + 4*v_2N - v_3N)/ex.dy**3
-                    
+            if space[j-1,i] == -1:
+                u_S = ex.interp_S(i,j, i,j-1,u_k)
+                v_S = ex.interp_S(i,j, i,j-1,v_k)
             else: 
-
                 u_S = u[(j-1)*n + i]
                 v_S = v[(j-1)*n + i]
-                
-                u_N = u[(j+1)*n + i]
-                v_N = v[(j+1)*n + i]
-                
-                uyy_k = (u_N -2*u_k + u_S)/ex.dy**2
-                vyy_k = (v_N -2*v_k + v_S)/ex.dy**2
+
+            uyy_k = (u_N -2*u_k + u_S)/ex.dy**2
+            vyy_k = (v_N -2*v_k + v_S)/ex.dy**2
                 
             px[k] = uxx_k + uyy_k
             py[k] = vxx_k + vyy_k
+            
     return px, py
 
 
