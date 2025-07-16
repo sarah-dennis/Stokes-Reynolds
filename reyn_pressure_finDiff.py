73b4e6527e0f0154bd6f4a5abfb612b5760796f3
Sarah Dennis <sarahdennis@brandeis.edu>
2025-07-16 08:35:23 -0400

    slight

--- reyn_pressure_finDiff.py
+++ reyn_pressure_finDiff.py
@@ -10,22 +10,14 @@ import numpy as np
 # Reynolds rhs
 def make_rhs(height):
     N = height.Nx
-# <<<<<<< Updated upstream
-# =======
-#     c = 6 * height.visc * height.U/(2*height.dx)
 
-# >>>>>>> Stashed changes
     rhs = np.zeros(N)    
-    
-    # c = 6 * height.visc * height.U/ (2*height.dx)
-    # for i in range(1,N-1):
-    #     rhs[i] = c*(height.hs[i+1]-height.hs[i-1])
-    
+
     rhs = 6 * height.visc * height.U * height.hxs
-    
     
     rhs[0] = height.p0
     rhs[N-1] = height.pN
+    
     return rhs
 
 def make_mat(height):
@@ -42,7 +34,7 @@ def make_mat(height):
         hr = height.hs[(i+1) % N]
 
         #P(i) central diagonal
-# <<<<<<< Updated upstream
+
         D_center[i] = -(hr**3 + 2*hc**3 + hl**3) 
         
         #P(i+1) upper diagonal
@@ -50,15 +42,7 @@ def make_mat(height):
         
         #P(i-1) lower diagonal
         D_lower[i] = (hl**3 + hc**3)
-# =======
-#         D_center[i] = -(hr**3 + 2*hc**3 + hl**3) /(2*(height.dx**2)) 
-        
-#         #P(i+1) upper diagonal
-#         D_upper[i] = (hr**3 + hc**3) /(2*(height.dx**2))
-        
-#         #P(i-1) lower diagonal   
-#         D_lower[i] = (hl**3 + hc**3) /(2*(height.dx**2))        # 
-# >>>>>>> Stashed changes
+
         
     # combine as upper, middle, lower diagonals
     D = np.diagflat(D_center) + np.diagflat(D_lower[1:N], -1) + np.diagflat(D_upper[0:N-1], 1)
